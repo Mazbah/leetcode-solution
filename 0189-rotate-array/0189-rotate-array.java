ee677e241b9b55deed9fcc448d1c216e7a04a1f6
Md. Mazbah Uddin Ahammed Chy <mazbah142@gmail.com>
2025-01-12 21:59:35 +0900

    Time: 1 ms (50.32%), Space: 57.5 MB (46.12%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,40 +4,40 @@ class Solution {
         // Brute force -- time-0(n+d) ,, space-O(n) - jeheto extra ekta array newa hoiche
 
         int n = nums.length;
-        // int diff = n-(k%n)-1;
-        // int a[] = new int[n];
-        // int j=0;
-        // for(int i=diff;i>=0;i--){
-        //    a[j] = nums[i];
-        //    j++; 
-        // }
-        // for(int i=n-1;i>diff;i--){
-        //    a[j] = nums[i];
-        //    j++; 
-        // }
-        // int x=n-1;
-        // for(int i=0;i<n;i++){
-        //     nums[i] = a[x];
-        //     x--;
-        // }
+        int diff = n-(k%n)-1;
+        int a[] = new int[n];
+        int j=0;
+        for(int i=diff;i>=0;i--){
+           a[j] = nums[i];
+           j++; 
+        }
+        for(int i=n-1;i>diff;i--){
+           a[j] = nums[i];
+           j++; 
+        }
+        int x=n-1;
+        for(int i=0;i<n;i++){
+            nums[i] = a[x];
+            x--;
+        }
 
 
         // Better approach
 
-        int d = k%n;
-        reverse(nums, 0, n-1); 
-        reverse(nums, 0, d-1); 
-        reverse(nums, d, n-1);
+        // int d = k%n;
+        // reverse(nums, 0, n-1); 
+        // reverse(nums, 0, d-1); 
+        // reverse(nums, d, n-1);
     }
 
-    public void reverse(int[] nums, int start, int end){
-        while(start < end){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
+    // public void reverse(int[] nums, int start, int end){
+    //     while(start < end){
+    //         int temp = nums[start];
+    //         nums[start] = nums[end];
+    //         nums[end] = temp;
 
-            start++;
-            end--;
-        }  
-    }
+    //         start++;
+    //         end--;
+    //     }  
+    // }
 }
