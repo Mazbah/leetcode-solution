ad746b0bdb3e69fc47da07e2c21cb1fa512f94fc
Md. Mazbah Uddin Ahammed Chy <mazbah142@gmail.com>
2025-01-13 19:01:31 +0900

    Time: 0 ms (100%), Space: 45.7 MB (11.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,29 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int x=-1;
+
+        // Time Complexity Huge 
+        // Arrays.sort(nums);
+        // int x=-1;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i] != i){
+        //         x = i;
+        //         break;
+        //     } 
+        // }
+        // if(x==-1){
+        //     x = nums.length;
+        // }
+
+        int sumTotal = 0;
+        int sumNums = 0;
+
+        for(int i=0;i<=nums.length;i++){
+            sumTotal +=i;
+        }
+
         for(int i=0;i<nums.length;i++){
-            if(nums[i] != i){
-                x = i;
-                break;
-            } 
+            sumNums +=nums[i];
         }
-        if(x==-1){
-            x = nums.length;
-        }
-        return x;
+        return sumTotal-sumNums;
     }
 }
