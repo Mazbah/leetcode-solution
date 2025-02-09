c585943ed3cd04c913cf0499074e8476f16d3921
Md. Mazbah Uddin Ahammed Chy <mazbah142@gmail.com>
2025-02-09 11:59:39 +0900

    Time: 13 ms (18.53%), Space: 44.8 MB (96.56%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,26 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
+
+        // Bitwise Solution
+        // int result = 0;
+        // for(int i=0;i<nums.length;i++){
+        //     result ^= nums[i];
+        // }
+        // return result;
+
+        // HashMap Solution
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
         for(int i=0;i<nums.length;i++){
-            result ^= nums[i];
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
         }
-        return result;
+
+        for(Map.Entry<Integer, Integer> it : hm.entrySet()) {
+            if (it.getValue() == 1) {
+                return it.getKey();
+            }
+        }
+
+        return -1;
     }
 }
