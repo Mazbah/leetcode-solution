4bfea28ccba8133dfa2495e3da3e831feefbed53
Md. Mazbah Uddin Ahammed Chy <mazbah142@gmail.com>
2025-02-27 22:52:26 +0900

    Time: 1 ms (89.68%), Space: 44.9 MB (71.69%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -2,14 +2,31 @@ class Solution {
     public int maxProduct(int[] nums) {
         int product= nums[0];
         int n = nums.length;
+        // Brute Force
+        // for(int i=0;i<n;i++){
+        //     int x = nums[i];
+        //     for(int j=i+1;j<n;j++){
+        //          x *= nums[j];
+        //         if(x>0) product = Math.max(product,x);
+        //         else    product = Math.max(product, nums[j]);
+        //     }
+        // }
+
+        // Observation based
+        
+        int suffix=1;
+        int prefix=1;
+
         for(int i=0;i<n;i++){
-            int x = nums[i];
-            for(int j=i+1;j<n;j++){
-                 x *= nums[j];
-                if(x>0) product = Math.max(product,x);
-                else    product = Math.max(product, nums[j]);
-            }
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+
+            product = Math.max(product, Math.max(prefix, suffix));
         }
+
         return product;
     }
 }
