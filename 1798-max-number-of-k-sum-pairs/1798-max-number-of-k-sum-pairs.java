a84b1fd43a71a3e5db88f1d6da045f49bb250ef1
Md. Mazbah Uddin Ahammed Chy <mazbah142@gmail.com>
2025-07-30 21:44:59 +0900

    Time: 46 ms (7.62%), Space: 58.3 MB (11.49%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -1,35 +1,37 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
         int n = nums.length-1;
-        int i=0;
+        // int i=0;
         int count = 0;
-        Arrays.sort(nums);
 
-        while(n>i){
-            if(nums[i]+nums[n] == k){
+        // Two pointer approach
+        // Arrays.sort(nums);
+
+        // // while(n>i){
+        // //     if(nums[i]+nums[n] == k){
+        // //         count++;
+        // //         i++;
+        // //         n--;
+        // //     }else if(nums[i]+nums[n] > k){
+        // //         n--;
+        // //     }else{
+        // //         i++;
+        // //     }
+        // // }
+
+
+        // HashMap
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+      
+        for(int num: nums){
+            if(map.containsKey(num) && map.get(num)>0){ // khutechi value tai, key+value = num
                 count++;
-                i++;
-                n--;
-            }else if(nums[i]+nums[n] > k){
-                n--;
+                map.put(num, map.get(num)-1);
             }else{
-                i++;
+                map.put(k-num, map.getOrDefault(k-num,0)+1); // Initially insert kortechi key te k-num
             }
         }
-
-        // HashMap<Integer, Integer> map = new HashMap<>();
-        // int count = 0;
-        // for(int i=0;i<nums.length;i++){
-        //     map.put(nums[i], k-nums[i]);
-        // }
-
-        // for(int i=0;i<nums.length;i++){
-        //     int x = k-nums[i];
-        //     if(map.containsKey(x) && map.get(x)>0){
-        //         count++;
-        //         map.put(x, map.get(x))
-        //     }
-        // }
 
         return count;
     }
