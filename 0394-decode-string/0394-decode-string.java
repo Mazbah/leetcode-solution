47e3c0bdcbfd5c920ab4a31ab0c8fd61431e86e9
Md. Mazbah Uddin Ahammed Chy <mazbah142@gmail.com>
2025-08-10 11:00:43 +0900

    Time: 0 ms (100%), Space: 41.7 MB (40.07%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,33 +1,63 @@
 class Solution {
     public String decodeString(String s) {
+        // Stack<Integer> numSt = new Stack<>();
+        // Stack<StringBuilder> charSt = new Stack<>();
+        
+        // StringBuilder sb = new StringBuilder();
+
+        // int num = 0;
+        // for(char ch: s.toCharArray()){
+
+        //     if(Character.isDigit(ch)){
+        //         num = num*10 + (ch-'0');
+        //     }else if(ch == '['){    
+        //         numSt.push(num);
+        //         num = 0;
+        //         charSt.push(sb);   // '[' asle always eitar agher value sb te add
+        //         sb = new StringBuilder();
+        //     }else if(ch == ']'){ 
+        //         int k = numSt.pop();
+        //         StringBuilder temp = sb;   //current temp sb;
+        //         sb = charSt.pop();  // agher stach theke value ene eitar shate add
+        //         for(int i=0;i<k;i++){
+        //             sb.append(temp);
+        //         }
+        //     }
+        //     else{
+        //         sb.append(ch);
+        //     }          
+        // }
+
+        // return sb.toString();    
+
         Stack<Integer> numSt = new Stack<>();
         Stack<StringBuilder> charSt = new Stack<>();
-        
+
         StringBuilder sb = new StringBuilder();
 
         int num = 0;
-        for(char ch: s.toCharArray()){
-
-            if(Character.isDigit(ch)){
-                num = num*10 + (ch-'0');
-            }else if(ch == '['){    
-                numSt.push(num);
-                num = 0;
-                charSt.push(sb);   // '[' asle always eitar agher value sb te add
-                sb = new StringBuilder();
-            }else if(ch == ']'){ 
-                int k = numSt.pop();
-                StringBuilder temp = sb;   //current temp sb;
-                sb = charSt.pop();  // agher stach theke value ene eitar shate add
-                for(int i=0;i<k;i++){
-                    sb.append(temp);
+        for(char c: s.toCharArray()){
+            if(Character.isDigit(c)){
+                num = num*10 + (c-'0');
+            }else{
+                if(c == '['){
+                    numSt.push(num);
+                    num = 0;
+                    charSt.push(sb);
+                    sb = new StringBuilder();
+                }else if(c == ']'){
+                    int k = numSt.pop();
+                    StringBuilder temp = sb;
+                    sb = charSt.pop();
+                    for(int i=0;i<k;i++){
+                        sb.append(temp);
+                    }
+                }else{
+                    sb.append(c);
                 }
             }
-            else{
-                sb.append(ch);
-            }          
         }
 
-        return sb.toString();    
+        return sb.toString();
     }
 }
