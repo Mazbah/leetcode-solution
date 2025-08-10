93156c1da1b39593d219e938eaade6e9e7d24161
Md. Mazbah Uddin Ahammed Chy <mazbah142@gmail.com>
2025-08-10 11:25:22 +0900

    Time: 2 ms (96.57%), Space: 42 MB (31.64%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,26 +2,19 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
 
-        Boolean x = false;
+        Boolean x = true;
         for(char c: s.toCharArray()){
             if(c == '(' || c == '{' || c == '[' ){
                 st.push(c);
             }else{
                 if(!st.isEmpty()){
                     char ch = st.pop();
-                    if(c == ')' && ch == '(' ){
-                        x = true;
-                    }else if(c == '}' && ch == '{' ){
-                        x = true;
-                    }else if(c == ']' && ch == '[' ){
-                        x = true;
-                    }else{
-                        x = false;
-                        break;
-                    }
+                    if(c == ')' && ch == '(' ) continue;
+                    else if(c == '}' && ch == '{' ) continue;
+                    else if(c == ']' && ch == '[' ) continue;
+                    else return false;
                 }else{
-                    x = false;
-                    break;
+                    return false;
                 }
             }
         }
