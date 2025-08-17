2f6f90bd574fe1cccb0d73599c03274bba05ac46
Md. Mazbah Uddin Ahammed Chy <mazbah142@gmail.com>
2025-08-17 12:26:57 +0900

    Time: 0 ms (100%), Space: 42 MB (8.24%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,24 +1,36 @@
 class Solution {
     public int minAddToMakeValid(String s) {
         
-        Stack<Character> st = new Stack<>();
-        st.push('p');
+        // Stack<Character> st = new Stack<>();
+        // st.push('p');
+
+        // for(int i=0;i<s.length();i++){
+        //     char ch = s.charAt(i);
+        //     if(ch == ')'){
+        //         if(st.peek() == '('){
+        //             st.pop();
+        //         }else{
+        //             st.push(ch);
+        //         }
+        //     }else{
+        //         st.push(ch);
+        //     }
+        // }
+        // return st.size()-1;
+
+        int opening =0, closing = 0;
 
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(ch == ')'){
-                if(st.peek() == '('){
-                    st.pop();
-                }else{
-                    st.push(ch);
-                }
+            if(ch == '('){
+                opening++;
+            }else if(ch == ')' && opening>0){
+                opening--;
             }else{
-            
-                    st.push(ch);
-                
+                closing++;
             }
         }
 
-        return st.size()-1;
+        return opening+closing;
     }
 }
